4d8afd4b1ffbd9200b7c80041aad8cff54d0892c
Marcos Guerreiro Rebelo <37541973+marcosrebelo97@users.noreply.github.com>
2023-06-29 11:33:20 -0400

    organizando arquivos

--- enderco/src/main/java/com/gestaopessoa/enderco/Repository/EnderecoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gestaopessoa.enderco.Repository;
-
-import com.gestaopessoa.enderco.Entities.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-}
--- enderco/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO tb_pessoa (nome, data_nascimento) VALUES ('Cicero','01/06/1996')
-INSERT INTO tb_pessoa (nome, data_nascimento) VALUES ('Higor','01/06/1996')
-
-INSERT INTO tb_endereco(logradouro, cep, numero, cidade, pessoa_id) VALUES ('Rua X', '69077769', '20', 'Manaus', '1')
