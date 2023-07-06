871f127b34752ffed601fdd0752e2371b5988bef
Marcos Guerreiro Rebelo <37541973+marcosrebelo97@users.noreply.github.com>
2023-07-06 15:27:51 -0400

    delete pacote duplicados

--- src/main/java/com/gestaopessoa/enderco/Repository/PessoaEnderecoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gestaopessoa.enderco.Repository;
-
-import com.gestaopessoa.enderco.Entities.PessoaEndereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PessoaEnderecoRepository extends JpaRepository<PessoaEndereco, Long> {
-}
